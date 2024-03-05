9c2f0539e64527e2f96ab37e19c490715d4f7e93
khaihoang110 <khaihoang119nkh@gmail.com>
2024-03-05 13:27:19 +0700

    demo

---
+++ Demo/session_01/dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+var strTS = "welcome to this course";
+console.log(strTS);
+var strTS1 = "welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course";
+console.log(strTS1);
+function getvalue() {
+}
---
+++ Demo/session_01/src/app.ts
@@ -0,0 +1,7 @@
+let strTS: string = "welcome to this course";
+console.log(strTS);
+let strTS1: string = "welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course welcome to this course" ;
+console.log(strTS1);
+function getvalue() {
+    
+}
